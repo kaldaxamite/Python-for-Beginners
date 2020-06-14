6b185daf8d86800a927e12de21379f89f8e09a6e
debforcode <66866273+debforcode@users.noreply.github.com>
2020-06-14 12:36:49 +0530

    seventh

--- HelloWorld/app3.py
+++ HelloWorld/app3.py
@@ -1,3 +1,4 @@
 name = input('what is your name? ')
-print('Hi ' + name)
+favorite_colour = input('what is your favorite colour? ')
+print(name + 'likes' + favorite_colour)
 
---
+++ HelloWorld/app4.py
@@ -0,0 +1,3 @@
+birth_year = input('Birth year: ')
+age = 2019 - birth_year
+print(age)
