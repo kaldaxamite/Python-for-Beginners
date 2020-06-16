fa18075116066243f8acc3dd13cd21591422bb99
debforcode <66866273+debforcode@users.noreply.github.com>
2020-06-16 13:01:57 +0530

    Thirty-forth

---
+++ HelloWorld/app16.py
@@ -0,0 +1,8 @@
+name = "debayan nada "
+
+if len(name) < 3:
+    print("Name must be at least 3 character")
+elif len(name) > 50:
+    print("Name must be a maximum of 50 charachers.")
+else:
+    print("Name looks good!")
