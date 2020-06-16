a854298d8b11dd152126bfad2e2afd11d07c465d
debforcode <66866273+debforcode@users.noreply.github.com>
2020-06-16 12:49:40 +0530

    Thirty-second

--- HelloWorld/app14.py
+++ HelloWorld/app14.py
@@ -1,4 +1,4 @@
-has_high_income = True
-has_good_credit = False
-if has_high_income or has_good_credit:
+has_good_credit = True
+has_criminal_record = False
+if has_good_credit and not has_criminal_record:
     print("Eligible for loan")
