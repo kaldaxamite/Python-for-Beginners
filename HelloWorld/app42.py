c2a434030c127f585fe4314ccb21b1a915e6b05b
MichaelDemiurgos <MichaelDemiurgos@users.noreply.github.com>
2020-08-09 12:45:17 +0530

    Sixty-ninth

---
+++ HelloWorld/app42.py
@@ -0,0 +1,10 @@
+message = input(">")
+words = message.split(' ')
+emojis = {
+    ":)": "happy"
+    ":(": "sad"
+}
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+print(output)
