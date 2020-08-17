cb4f6418ca6e1c0d1c54804fe0f4f84e97309571
MichaelDemiurgos <MichaelDemiurgos@users.noreply.github.com>
2020-08-17 16:17:14 +0530

    Seventh-third

--- HelloWorld/app45.py
+++ HelloWorld/app45.py
@@ -1,10 +1,9 @@
-def greet_user(name):
-    print(f'Hi {name}!')
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name} {last_name}!')
     print('Welcome aboard')
 
 
 print("Start")
-greet_user("John")
-greet_user("Mary")
+greet_user("John", "Smith")
 print("Finish")
 
