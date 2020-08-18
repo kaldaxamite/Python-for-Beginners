b4697c3b931844e723305c067ce61360ab536d15
MichaelDemiurgos <MichaelDemiurgos@users.noreply.github.com>
2020-08-18 14:03:49 +0530

    Seventh-eight

--- HelloWorld/app42.py
+++ HelloWorld/app42.py
@@ -1,10 +1,10 @@
 message = input(">")
-words = message.split(' ')
+word = message.split(" ")
 emojis = {
-    ":)": "happy"
+    ":)": "smile",
     ":(": "sad"
 }
 output = ""
 for word in words:
-    output += emojis.get(word, word) + " "
+    output += emojis.get*(word, word) + " "
 print(output)
