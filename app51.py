eb327a9d3a48ea70b9de4674fac27ef8fd4387ae
Debayan <66866273+MichaelDemiurgos@users.noreply.github.com>
2020-08-30 13:22:22 +0530

    Eighty-seventh

--- app51.py
+++ app51.py
@@ -1,6 +1,15 @@
 class Point:
+    def ___init___(self, x, y):
+        self.x = x
+        self.y = y
+
     def move(self):
         print("move")
 
     def draw(self):
         print("draw")
+
+
+point = Point(10, 20)
+point.x = 11
+print(point.x)
